653b7e630f57c0971a20469805a2c1d9c67a69bb
sakklavut <sakklavut23395@gmail.com>
2023-04-19 13:42:01 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,85 @@
+<div class="header">
+     <div class="container">
+          <div class="row">
+               <div class="col-xl-12">
+                    <ul class="d-flex justify-content-end header_nemu">
+                         <li class="me-1">
+                              <a class="nav-link active" aria-current="page" href="#">บุคคลากร</a>
+                         </li>
+                         <li class="me-1">
+                              <a class="nav-link" href="#">ส่วนงาน</a>
+                         </li>
+                         <li class="me-1">
+                              <a class="nav-link" href="#">หอสมุดและคลังความรู้</a>
+                         </li>
+                         <li class="me-1">
+                              <a class="nav-link" href="#">แนะนำและติชม</a>
+                         </li>
+                         <li class="nav-item">
+                              <div class="d-flex">
+                                   <div>
+                                        <a class="nav-link" href="#">ก</a>
+                                   </div>
+                                   <div>
+                                        <a class="nav-link" href="#">ก</a>
+                                   </div>
+                                   <div>
+                                        <a class="nav-link" href="#">ก</a>
+                                   </div>
+                              </div>
+                         </li>
+                         <li class="me-1">
+                              <div class="d-flex">
+                                   <div>
+                                        <a class="nav-link" href="#">ก-</a>
+                                   </div>
+                                   <div>
+                                        <a class="nav-link" href="#">ก+</a>
+                                   </div>
+                              </div>
+                         </li>
+                         <li class="me-1">
+                              <a href=""><img src="icon/icon_en.svg" alt=""></a>  
+                         </li>
+                         <li class="me-1">
+                              <a href=""><img src="icon/search.svg" alt=""></a>  
+                         </li>
+                    </ul>
+               </div>
+          </div>    
+     </div>
+</div>
+
+
+
+<!-- <div class="d-flex justify-content-end">
+               <div>
+                    <ul class=" d-flex header_nemu">
+                         <li class="nav-item">
+                              <a class="nav-link active" aria-current="page" href="">บุคคลากร</a>  
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link" href="">ส่วนงาน</a>  
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link" href="">หอสมุดและคลังความรู้</a>  
+                         </li>
+                         <li class="nav-item">
+                              <a class="nav-link"  href="">แนะนำและติชม</a>  
+                         </li>
+                        
+                         <li>
+                              <a href="">ก-</a>  
+                         </li>
+                         <li>
+                              <a href="">ก+</a>  
+                         </li>
+                         <li class="nav-item">
+                              <a href=""><img src="icon/icon_en.svg" alt=""></a>  
+                         </li>
+                         <li class="nav-item">
+                              <a href=""><img src="icon/search.svg" alt=""></a>  
+                         </li>
+                    </ul>
+               </div>
+          </div> -->
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('comtop.php'); ?>
+<?php include('header.php'); ?>
+
+<?php include('combottom.php'); ?>
+
+
+
+
+
