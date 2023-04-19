f2730b8fd8a4adff1fcb28a4f409c7ef6144d933
sakklavut <sakklavut23395@gmail.com>
2023-04-19 15:40:13 +0700

    add header

--- header.php
+++ header.php
@@ -1,85 +1,56 @@
 <div class="header">
      <div class="container">
-          <div class="row">
-               <div class="col-xl-12">
-                    <ul class="d-flex justify-content-end header_nemu">
-                         <li class="me-1">
+          <ul class="d-flex  header_nemu">
+               <div class="box_menu">
+                    <div class="box_sup_1 d-flex">
+                         <li class="box_li">
                               <a class="nav-link active" aria-current="page" href="#">บุคคลากร</a>
                          </li>
-                         <li class="me-1">
+                         <li class="box_li">
                               <a class="nav-link" href="#">ส่วนงาน</a>
                          </li>
-                         <li class="me-1">
+                         <li class="box_li">
                               <a class="nav-link" href="#">หอสมุดและคลังความรู้</a>
                          </li>
-                         <li class="me-1">
+                         <li class="box_li">
                               <a class="nav-link" href="#">แนะนำและติชม</a>
                          </li>
-                         <li class="nav-item">
-                              <div class="d-flex">
-                                   <div>
+                    </div>
+                    <div class="box_sup_2 d-flex">
+                         <li class="box_li">
+                              <div class="d-flex justify-content-center">
+                                   <div class="d-flex">
+                                        <div class="me-1">
+                                             <a class="nav-link" href="#">ก</a>
+                                        </div>
+                                   <div class="me-1">
                                         <a class="nav-link" href="#">ก</a>
                                    </div>
-                                   <div>
-                                        <a class="nav-link" href="#">ก</a>
-                                   </div>
-                                   <div>
-                                        <a class="nav-link" href="#">ก</a>
+                                   <div class="me-1">
+                                             <a class="nav-link" href="#">ก</a>
                                    </div>
                               </div>
                          </li>
-                         <li class="me-1">
-                              <div class="d-flex">
-                                   <div>
-                                        <a class="nav-link" href="#">ก-</a>
-                                   </div>
-                                   <div>
-                                        <a class="nav-link" href="#">ก+</a>
+                         <li class="box_li">
+                              <div class="d-flex justify-content-center">
+                                   <div class="d-flex">
+                                        <div class="me-1">
+                                             <a class="nav-link" href="#">ก-</a>
+                                        </div>
+                                        <div class="me-1">
+                                             <a class="nav-link" href="#">ก+</a>
+                                        </div>
                                    </div>
                               </div>
                          </li>
-                         <li class="me-1">
-                              <a href=""><img src="icon/icon_en.svg" alt=""></a>  
+                         <li class="box_li">
+                              <a href=""><img class="icon_en" src="icon/icon_en.svg" alt=""></a>  
                          </li>
-                         <li class="me-1">
-                              <a href=""><img src="icon/search.svg" alt=""></a>  
+                         <li class="box_li">
+                              <a href=""><img class="icon_search" src="icon/search.svg" alt=""></a>  
                          </li>
-                    </ul>
+                    </div>
                </div>
-          </div>    
+          </ul>
      </div>
 </div>
-
-
-
-<!-- <div class="d-flex justify-content-end">
-               <div>
-                    <ul class=" d-flex header_nemu">
-                         <li class="nav-item">
-                              <a class="nav-link active" aria-current="page" href="">บุคคลากร</a>  
-                         </li>
-                         <li class="nav-item">
-                              <a class="nav-link" href="">ส่วนงาน</a>  
-                         </li>
-                         <li class="nav-item">
-                              <a class="nav-link" href="">หอสมุดและคลังความรู้</a>  
-                         </li>
-                         <li class="nav-item">
-                              <a class="nav-link"  href="">แนะนำและติชม</a>  
-                         </li>
-                        
-                         <li>
-                              <a href="">ก-</a>  
-                         </li>
-                         <li>
-                              <a href="">ก+</a>  
-                         </li>
-                         <li class="nav-item">
-                              <a href=""><img src="icon/icon_en.svg" alt=""></a>  
-                         </li>
-                         <li class="nav-item">
-                              <a href=""><img src="icon/search.svg" alt=""></a>  
-                         </li>
-                    </ul>
-               </div>
-          </div> -->
