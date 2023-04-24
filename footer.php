cd5c6741a0caa6040846ecac9f4ded78a169185d
sakklavut <sakklavut23395@gmail.com>
2023-04-24 11:01:26 +0700

    footer

---
+++ footer.php
@@ -0,0 +1,248 @@
+<div class="footer">
+     <div class="container">
+          <div class="row">
+               <div class="col-xl-3 mt-3">
+                    <div class="row">
+                         <div class="col-xl-12">
+                              <p class="h_footer">ติดต่อเรา</p>
+                              <div class="line_footer"></div>
+                         </div>
+                    </div>
+                    <div class="row">
+                         <div class="col-xl-12">
+                             <div class="d-flex">
+                                   <div class="me-2">
+                                        <img src="icon/map.svg" alt="">
+                                   </div>
+                                   <div class="me-2">
+                                        <p class="p_footer">
+                                             สถาบันพระบรมราชชนก กระทรวงสาธารณสุข อาคาร 4 ชั้น 8 สำนักงานปลัดกระทรวงสาธารณสุข ถนนติวานนท์ตำบลตลาดขวัญ  อำเภอเมือง จังหวัดนนทบุรี 11000
+                                        </p>
+                                   </div>
+                             </div>
+                         </div>
+                    </div>
+                    <div class="row">
+                         <div class="col-xl-12">
+                             <div class="d-flex">
+                                   <div class="me-2">
+                                        <img src="icon/tem.svg" alt="">
+                                   </div>
+                                   <div class="me-2">
+                                        <p class="p_footer">
+                                             02-590-1918
+                                        </p>
+                                   </div>
+                             </div>
+                         </div>
+                    </div>
+                    <div class="row">
+                         <div class="col-xl-12">
+                             <div class="d-flex">
+                                   <div class="me-2">
+                                        <img src="icon/mail.svg" alt="">
+                                   </div>
+                                   <div class="me-2">
+                                        <p class="p_footer">
+                                               saraban@pi.ac.th
+                                        </p>
+                                   </div>
+                             </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-xl-6 mt-3">
+                         <div class="row">
+                              <div class="col-xl-12">
+                                   <p class="h_footer">ระบบสารสนเทศ</p>
+                                   <div class="line_footer"></div>
+                              </div>
+                         </div>
+                         <div class="row">
+                              <div class="col-xl-12">
+                                   <div class="row">
+                                        <div class="col-xl-6">
+                                             <ul class="ul_footer">
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#">ระบบบุคลากร สํานักงานปลัดกระทรวงสาธารณสุข</a>
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#"> ระบบชำระเงินเพื่อขึ้นทะเบียนนักศึกษา</a>
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                       <a href="#"> ระบบรายงานผู้บริหาร (EIS)</a>  
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#">  ระบบขอเดินทางไปราชการ</a>    
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                             </ul>
+                                        </div>
+                                        <div class="col-xl-6">
+                                             <ul class="ul_footer">
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#">ระบบบุคลากร/ระบบสารบรรณ/ระบบลา</a>    
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#">ระบบชำระเงินเพื่อขึ้นทะเบียนนักศึกษา</a>   
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                      <a href="#">ระบบรายงานผู้บริหาร (EIS)</a>
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                                  <li class="li_footer">
+                                                       <div class="d-flex">
+                                                            <div class="me-2">
+                                                                 <img src="icon/li.svg" alt="">
+                                                            </div>
+                                                            <div class="me-2">
+                                                                  <p class="p_footer">
+                                                                       <a href="#"> ระบบขอเดินทางไปราชการ</a>
+                                                                   </p>
+                                                            </div>
+                                                       </div> 
+                                                  </li>
+                                             </ul>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               <div class="col-xl-3 mt-3">  
+                    <div class="row">
+                         <div class="col-xl-12">
+                              <p class="h_footer">ลิงก์ที่เกี่ยวข้อง</p>
+                              <div class="line_footer"></div>
+                         </div>
+                    </div>
+                    <div class="row">
+                         <div class="col-xl-12">
+                              <ul class="ul_footer">
+                                   <li class="li_footer">
+                                        <div class="d-flex">
+                                             <div class="me-2">
+                                                  <img src="icon/li.svg" alt="">
+                                             </div>
+                                             <div class="me-2">
+                                                  <p class="p_footer">
+                                                       <a href="#"> แผนที่มหาวิทยาลัย</a>
+                                                  </p>
+                                             </div>
+                                        </div> 
+                                   </li>
+                                   <li class="li_footer">
+                                        <div class="d-flex">
+                                             <div class="me-2">
+                                                  <img src="icon/li.svg" alt="">
+                                             </div>
+                                             <div class="me-2">
+                                                  <p class="p_footer">
+                                                       <a href="#"> คำถามที่พบบ่อย</a>
+                                                  </p>
+                                             </div>
+                                        </div> 
+                                   </li>
+                                   <li class="li_footer">
+                                        <div class="d-flex">
+                                             <div class="me-2">
+                                                  <img src="icon/li.svg" alt="">
+                                             </div>
+                                             <div class="me-2">
+                                                  <p class="p_footer">
+                                                       <a href="#"> สายตรงอธิการบดี</a>
+                                                  </p>
+                                             </div>
+                                        </div> 
+                                   </li>
+                                   <li class="li_footer">
+                                        <div class="d-flex">
+                                             <div class="me-2">
+                                                  <img src="icon/li.svg" alt="">
+                                             </div>
+                                             <div class="me-2">
+                                                  <p class="p_footer">
+                                                       <a href="#"> ITA ของสถาบัน</a>
+                                                  </p>
+                                             </div>
+                                        </div> 
+                                   </li>
+                                   <li class="li_footer">
+                                        <div class="d-flex">
+                                             <div class="me-2">
+                                                  <img src="icon/li.svg" alt="">
+                                             </div>
+                                             <div class="me-2">
+                                                  <p class="p_footer">
+                                                       <a href="#"> สมัครอบรมออนไลน์</a>
+                                                  </p>
+                                             </div>
+                                        </div> 
+                                   </li>
+                              </ul>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
